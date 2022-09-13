ae53db0ad4bcc39c7a6000417953003337e7e8bf
Sihab-10 <sihabsarar10@gmail.com>
2022-09-13 20:01:28 +0600

    js variable recap added

---
+++ variable.js
@@ -0,0 +1,27 @@
+// number type variable
+var sunglassPrice = 120;
+// string type variable
+var specialOne = "Ariana Grande";
+// boolean type variable
+var isSingle = true;
+// two types of number : integer:120 ;
+// float, deciaml: 3.65;
+
+// variable naming convention: rules, best parctices
+/**
+ * 1. Single Word
+ * 2. no gap or space 
+ * 3. no quote 
+ * 4. no keyword
+ * 5. can not start with a number 
+ * 6. can not contain dash
+ * 7. should use camelCase 
+*/
+// Math operations: +, -,*,/
+// shorthand: +=, -=, ++, --
+// convert: parseInt, parseFloat,
+
+// toFixed
+
+// remainder : vagses: % 
+
