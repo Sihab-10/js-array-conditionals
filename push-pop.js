a9d1b6a6245e860745e67de0e12f6d23e4663107
Sihab-10 <sihabsarar10@gmail.com>
2022-09-15 11:04:30 +0600

    push-pop added

---
+++ push-pop.js
@@ -0,0 +1,29 @@
+var numbers = [78, 45, 98, 45];
+// use push to add element to an array as the last element of an array
+// numbers.push(68);
+// console.log(numbers);
+var friends = ['balam', 'kalam', 'salam'];
+friends.push('gelam');
+friends.push('pailam');
+// console.log(friends);
+
+// use pop to get rid of the last element
+// console.log(numbers);
+// numbers.pop();
+var element = numbers.pop();
+// console.log(element);
+
+
+//to add an array in the first index use unshift
+
+var fruits = ['banana', 'mango', 'orange'];
+console.log(fruits);
+fruits.unshift('lemon');
+console.log(fruits);
+
+// to remove an array in the first index use shift
+
+fruits.shift();
+console.log(fruits);
+
+
