d0ca0f06398f94a741a7a023dd709549f7e67c23
Sihab-10 <sihabsarar10@gmail.com>
2022-09-15 10:27:16 +0600

    array indexing added

---
+++ index.js
@@ -0,0 +1,12 @@
+var numbers = [45, 68, 78, 56, 89, 98];
+
+// 1. get element value by index
+var element = numbers[6];
+console.log(element);
+// 2. set element value by index
+numbers[1] = 77;
+numbers[2] = 44;
+console.log(numbers);
+// 3. find index of an element
+var positionIndex = numbers.indexOf(89);
+console.log(positionIndex);
