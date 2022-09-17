8ecaf2c6f0d37968c719a7b673f12b836159f4d9
Sihab-10 <sihabsarar10@gmail.com>
2022-09-17 11:45:15 +0600

    conditional in js added

---
+++ conditional.js
@@ -0,0 +1,35 @@
+var iphonePrice = 79000;
+var myBudget = 95000;
+// if iphonePrice is less than myBudget than i will buy it.
+/**
+ * if iphonePrice is less than my budget 
+ *  i will buy the iphone
+ * 
+ * 
+ * 
+ * if(condition){
+ *          will execute if the condition is true
+ * 
+ * }
+ * 
+ * */ 
+// if (iphonePrice < myBudget) {
+//     console.log("iphone kine futani marbo arki");
+// }
+
+var chikenPrice = 500;
+var myMoney = 50;
+// if (chikenPrice <= myMoney) {
+//     console.log('Murgir raan khamu ar haddi chibamu');
+// }
+// if (chikenPrice > myMoney) {
+//     console.log('smashed potato and lentils soup');
+// }
+
+
+if (chikenPrice < myMoney) {
+    console.log('I will eat chicken');
+}
+else {
+    console.log('I will eat potato');
+}
