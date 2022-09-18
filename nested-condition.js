22dccecb4f4ea2d9962ce675cf6f43dfd2ebb057
Sihab-10 <sihabsarar10@gmail.com>
2022-09-18 12:07:47 +0600

    nested condition in js added

---
+++ nested-condition.js
@@ -0,0 +1,15 @@
+var math = false;
+var geometry = true;
+var straightLine = false;
+if (math === true) {
+  if (geometry === true) {
+    if (straightLine === true) {
+    } else {
+      console.log("baka pothe jaba na");
+    }
+  } else {
+    console.log("pithagoras hoite parba na");
+  }
+} else {
+  console.log("tumi math sikha suru koro");
+}
