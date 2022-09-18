50d5f27bf4add2294fcd089416cf9e2d8fb29595
Sihab-10 <sihabsarar10@gmail.com>
2022-09-18 11:42:59 +0600

    multiple condition js added

---
+++ multiple-condition.js
@@ -0,0 +1,27 @@
+// var isGraduted = true;
+// var salary = 75000;
+// var cars = 2;
+// if (isGraduted == false) {
+//   console.log("eso biye kore feli");
+// } else {
+//   console.log("tor kopale biya nai");
+// }
+// if (isGraduted == true && salary >= 50000) {
+//   console.log("eso biye kore feli");
+// } else {
+//   console.log("tor kopale biya nai");
+// }
+// if (isGraduted == true && salary >= 50000 && cars >= 1) {
+//   console.log("eso biye kore feli");
+// } else {
+//   console.log("tor kopale biya nai");
+// }
+
+var isGraduted = false;
+var salary = 31000;
+var cars = 2;
+if (isGraduted === true || salary >= 50000 || cars >= 1) {
+  console.log("Eso prem kori");
+} else {
+  console.log("tomar friend er mobile number deo");
+}
