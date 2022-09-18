6ab9175828f2c2955cddc71cead6abc4acb1f655
Sihab-10 <sihabsarar10@gmail.com>
2022-09-18 11:58:12 +0600

    multi stage condition js added

---
+++ multi-stage-condition.js
@@ -0,0 +1,13 @@
+var money = 10;
+var danishPrice = 45;
+var butterBread = 35;
+var toastBiscuit = 20;
+if (danishPrice < money) {
+  console.log("dan dan danish khamu");
+} else if (butterBread < money) {
+  console.log("butter bread khamu");
+} else if (toastBiscuit < money) {
+  console.log("toast biscuit khamu");
+} else {
+  console.log("khali cha ei sasther jonno valo");
+}
